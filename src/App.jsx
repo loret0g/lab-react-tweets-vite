60aefe8b5b5fee9c841254d2c25d81d6dc60c77e
Loreto <loreto.garde@gmail.com>
2024-09-22 15:30:50 +0200

    done

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,9 @@ const tweetsArray = [
 function App() {
   return (
     <div className="App">
-      <Tweet />
+      {tweetsArray.map((eachTweet, index) => (
+        <Tweet key={index} tweet={ eachTweet } />
+      ))}
     </div>
   );
 }
